7e3c2141f63aae051f782d70eea44d3c98d024b6
KonstantinG <alliwannadois1@mail.ru>
2021-02-25 18:37:44 +0300

    GSLab1

---
+++ src/main/java/com/geterogensystem/demo/GeterogenSystemLab1Application.java
@@ -0,0 +1,13 @@
+package com.geterogensystem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeterogenSystemLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeterogenSystemLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/geterogensystem/demo/GeterogenSystemLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geterogensystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeterogenSystemLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
